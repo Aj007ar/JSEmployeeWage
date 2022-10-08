beda3e2c73e02170ab20fc0ab819f76ec1f62604
Aj007ar <aj007.ar@gmail.com>
2022-10-08 17:22:44 +0530

    [Ajay] Added Employee Presenty

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,10 @@
 //Welcome to Employee Wage JS programs
+//UC 1 Check employee absent or present
+const IS_PRESENT=1
+const IS_ABSENT=0
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==IS_PRESENT){
+    console.log("Employee is Present");
+}else{
+    console.log("Employee is Absent");
+}
