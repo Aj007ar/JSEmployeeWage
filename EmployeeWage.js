7887b8d26537e61fb81d7362dfaee70e9ef34760
Aj007ar <aj007.ar@gmail.com>
2022-10-08 19:26:44 +0530

    [Ajay] Added Employee attendance.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,23 @@
 //Welcome to Employee Wage JS programs
-//UC 1 Check employee absent or present
-const IS_PRESENT=1
-const IS_ABSENT=0
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==IS_PRESENT){
+{
+    console.log("1.UC1");
+    var prompt=require("prompt-sync")();
+    let number=prompt("Enter which UC executed  : ");
+switch(number)
+{
+    case "1":
+    
+    //UC 1 Check employee absent or present
+    const IS_PRESENT=1
+    const IS_ABSENT=0
+    let empCheck=Math.floor(Math.random()*10)%2;
+    if(empCheck==IS_PRESENT){
     console.log("Employee is Present");
-}else{
+    }
+    else{
     console.log("Employee is Absent");
+    }
+        break;
+   
+}
 }
