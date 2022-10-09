9b1d3199ff21161f76bec08a5713bed03b876347
Aj007ar <aj007.ar@gmail.com>
2022-10-09 11:07:05 +0530

    [Ajay] Added Total monthly Wage.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,15 +5,26 @@
   const PART_TIME_HOURS = 4;
   const FULL_TIME_HOURS = 8;
   const WAGE_PER_HOUR = 20;
+  const NO_OF_WORKING_DAYS=20;
   let empHrs=0;
-
-    console.log("1.Employee Attendance\n2.Calculate Daily Wage\n3.Get Working hours");
+  //Get Working Hours function
+  function getWorkingHours(empCheck) {
+    switch (empCheck) {
+      case IS_PART_TIME:
+        return PART_TIME_HOURS;
+      case IS_FULL_TIME:
+        return FULL_TIME_HOURS;
+      default:
+        return 0;
+    }
+  }
+    console.log("1.Employee Attendance\n2.Calculate Daily Wage\n3.Get Working \n4.Calculate Employee wage for Month");
     var prompt=require("prompt-sync")();
     let number=prompt("Enter which UC executed  : ");
 switch(number)
 {
+//UC 1 Check employee absent or present
     case "1":   
-    //UC 1 Check employee absent or present
     const IS_PRESENT=1
     const IS_ABSENT=0
     let empCheck=Math.floor(Math.random()*10)%2;
@@ -24,6 +35,7 @@ switch(number)
     console.log("Employee is Absent");
     }
         break;
+//UC 2 Calculate Daily Wage
         case "2":
             console.log("---------UC 2--------------------")
             {
@@ -43,22 +55,24 @@ switch(number)
                 console.log("UC2-----EmpWage: "+empWage);
             }
             break;
+//UC 3 calculate daily wage with hour
             case "3":
-                {
-                    function getWorkingHours(empCheck) {
-                      switch (empCheck) {
-                        case IS_PART_TIME:
-                          return PART_TIME_HOURS;
-                        case IS_FULL_TIME:
-                          return FULL_TIME_HOURS;
-                        default:
-                          return 0;
-                      }
-                    }
+              {
                     let empCheck = Math.floor(Math.random() * 10) % 3;
                     let empHrs = getWorkingHours(empCheck);
                     let empWage = empHrs * WAGE_PER_HOUR;
                     console.log("UC3---> Hours:" + empHrs + "\nEmpWage:" + empWage);
                   }break;
+//UC 4 Calculate Monthly Wage
+            case "4":
+              {
+                  for(let day=0;day<NO_OF_WORKING_DAYS;day++)
+                  {
+                    let empCheck=Math.floor(Math.random()*10)%3;
+                    empHrs+=getWorkingHours(empCheck);
+                  }
+                  let empWage=empHrs*WAGE_PER_HOUR;
+                  console.log("Total Hours: "+empHrs+"\nEmp Wage: "+empWage);
+              }break;
 }
 }
